91ba3ca14c2101b22a5ed9b8e818f663689444ad
leopasch-dcsg <leonardo.paschoal@dcsg.com>
2026-09-11 02:27:57 -0300

    Adding fixes

--- src/sdds/common/util.py
+++ src/sdds/common/util.py
@@ -1,9 +1,36 @@
 from databricks.sdk.runtime import dbutils
+from pyspark.sql import SparkSession
+
+
+class NotebookUtil:
+
+    @staticmethod
+    def text_widget(param: str, default: str, label: str) -> None:
+        dbutils.widgets.text(param, default, label)
+
+    @staticmethod
+    def notebook_param(param: str, default: str | None = None) -> str | None:
+        try:
+            value = dbutils.widgets.get(param).strip()
+            if len(value) == 0:
+                value = default
+            return value
+        except Exception:
+            return default
+
+    @staticmethod
+    def spark_param(
+        spark: SparkSession, param: str, default: str | None = None
+    ) -> str | None:
+        return spark.conf.get(param, default)
+
 
 def get_param(param_name):
-    return dbutils.widgets.get(param_name)
+    return NotebookUtil.notebook_param(param_name)
+
 
 azure_secret_scope = get_param("azure_kv_scope")
+
 
 def get_secret(param_name):
     key = get_param(param_name)
